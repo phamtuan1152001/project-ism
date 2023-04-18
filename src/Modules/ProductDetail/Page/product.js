87e6226d3560d5aa1513606d3f88240b60219524
Nam Bobby <lethpnam27@gmail.com>
2023-04-18 17:42:10 +0700

    18/4

--- src/Modules/ProductDetail/Page/product.js
+++ src/Modules/ProductDetail/Page/product.js
@@ -1,13 +1,31 @@
 import React from "react"
+import "../bootstrap.scss";
+import Thumbnail1 from '@src/assets/images/gallery-1.jpg';
+import Thumbnail2 from '@src/assets/images/gallery-2.jpg';
+import Thumbnail3 from '@src/assets/images/gallery-3.jpg';
+import Thumbnail4 from '@src/assets/images/gallery-4.jpg';
+import Thumbnail5 from '@src/assets/images/ball.png';
+import Thumbnail6 from '@src/assets/images/book.png';
+import Thumbnail7 from '@src/assets/images/child.png';
+import Product1 from '@src/assets/images/product-1.jpg';
+import Product2 from '@src/assets/images/product-2.jpg';
+import Product3 from '@src/assets/images/product-3.jpg';
+import Product4 from '@src/assets/images/product-4.jpg';
+import Product5 from '@src/assets/images/product-5.jpg';
+import Product6 from '@src/assets/images/product-6.jpg';
+import Product7 from '@src/assets/images/product-7.jpg';
+import Product8 from '@src/assets/images/product-8.jpg';
+import Product9 from '@src/assets/images/product-9.jpg';
+import Product10 from '@src/assets/images/product-10.jpg';
 
 const Product = () => {
     return (
-        <>
+        <div className="productdetail">
             <div className="single-product">
                 <div className="col-2">
                     <div className="container">
                         <div className="main-image">
-                            <img src="TEST/images/gallery-1.jpg" width="100%" id="ProductImg" alt="Large Image"></img>
+                            <img src={Thumbnail1} width="100%" id="ProductImg" alt="Large Image"></img>
                         </div>
                         <div className="button-container">
                             <button id="prev-btn"><i className='bx bxs-chevron-left'></i></button>
@@ -15,13 +33,13 @@ const Product = () => {
                         </div>
                     </div>
                     <div className="small-img-row">
-                        <img src="TEST/images/gallery-1.jpg" width="100%" alt="Thumbnail 1"></img>
-                        <img src="TEST/images/gallery-2.jpg" width="100%" alt="Thumbnail 2"></img>
-                        <img src="TEST/images/gallery-3.jpg" width="100%" alt="Thumbnail 3"></img>
-                        <img src="TEST/images/gallery-4.jpg" width="100%" alt="Thumbnail 4"></img>
-                        <img src="TEST/images/gallery-4.jpg" width="100%" alt="Thumbnail 5"></img>
-                        <img src="TEST/images/gallery-4.jpg" width="100%" alt="Thumbnail 6"></img>
-                        <img src="TEST/images/gallery-4.jpg" width="100%" alt="Thumbnail 7"></img>
+                        <img src={Thumbnail1} width="100%" alt="Thumbnail 1"></img>
+                        <img src={Thumbnail2} width="100%" alt="Thumbnail 2"></img>
+                        <img src={Thumbnail3} width="100%" alt="Thumbnail 3"></img>
+                        <img src={Thumbnail4} width="100%" alt="Thumbnail 4"></img>
+                        <img src={Thumbnail5} width="100%" alt="Thumbnail 5"></img>
+                        <img src={Thumbnail6} width="100%" alt="Thumbnail 6"></img>
+                        <img src={Thumbnail7} width="100%" alt="Thumbnail 7"></img>
                     </div>
                     <div className="certification">
                         <p><i className='bx bxs-heart'></i>100% health guarantee for pets</p>
@@ -105,12 +123,12 @@ const Product = () => {
                 <div className="slider">
                     <h1>Our lovely customer</h1>
                     <div className="image-list">
-                        <a><img src="TEST/images/product-1.jpg" alt="" className="active"></img></a>
-                        <a><img src="TEST/images/product-2.jpg" alt=""></img></a>
-                        <a><img src="TEST/images/product-3.jpg" alt=""></img></a>
-                        <a><img src="TEST/images/product-4.jpg" alt=""></img></a>
-                        <a><img src="TEST/images/product-5.jpg" alt=""></img></a>
-                        <a><img src="TEST/images/product-6.jpg" alt=""></img></a>
+                        <a><img src={Product1} alt="" className="active"></img></a>
+                        <a><img src={Product2} alt=""></img></a>
+                        <a><img src={Product3} alt=""></img></a>
+                        <a><img src={Product4} alt=""></img></a>
+                        <a><img src={Product5} alt=""></img></a>
+                        <a><img src={Product6} alt=""></img></a>
                     </div>
                 </div>
                 <div className="image-navigation">
@@ -129,7 +147,7 @@ const Product = () => {
                     <div className="frames-container">
                         <div className="frame">
                             <div className="col-4">
-                                <img src="TEST/images/product-9.jpg"></img>
+                                <img src={Product7}></img>
                             </div>
                             <div className="row">
                                 <div className="row-text">
@@ -152,7 +170,7 @@ const Product = () => {
 
                         <div className="frame">
                             <div className="col-4">
-                                <img src="TEST/images/product-8.jpg"></img>
+                                <img src={Product8}></img>
                             </div>
                             <div className="row">
                                 <div className="row-text">
@@ -175,7 +193,7 @@ const Product = () => {
 
                         <div className="frame">
                             <div className="col-4">
-                                <img src="TEST/images/product-10.jpg"></img>
+                                <img src={Product9}></img>
                             </div>
                             <div className="row">
                                 <div className="row-text">
@@ -198,7 +216,7 @@ const Product = () => {
 
                         <div className="frame">
                             <div className="col-4">
-                                <img src="TEST/images/product-11.jpg"></img>
+                                <img src={Product10}></img>
                             </div>
                             <div className="row">
                                 <div className="row-text">
@@ -227,7 +245,7 @@ const Product = () => {
             <div className="end-text">
                 <p>� late 2022 Tahmid Ahmed. All Rights Reserved.</p>
             </div>
-        </>
+        </div>
     );
 };
 export default Product;
