099718032b09004ada5e544046866e9b979f19dd
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-01 14:02:38 +0700

    fix adding url

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,1 +1,8 @@
-export default [{ key: "Login" }, { key: "Home" }];
+export default [
+  { key: "Login" },
+  { key: "Home" },
+  { key: "ProductDetail" },
+  { key: "Category" },
+  { key: "Cart" },
+  { key: "Checkout" },
+];
