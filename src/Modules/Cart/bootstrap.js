9d68fcd617bda7678dd6ed6828f5dedb613b3c33
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-03 19:09:25 +0700

    fix

--- src/Modules/Cart/bootstrap.js
+++ src/Modules/Cart/bootstrap.js
@@ -1,29 +1,29 @@
-import intlMessagesEN from './i18n/localization/en.json';
-import intlMessagesVi from './i18n/localization/vi.json';
-import initReducer from './Store/initReducer';
-import initSagas from './Store/initSagas';
+import intlMessagesEN from "./i18n/localization/en.json";
+import intlMessagesVi from "./i18n/localization/vi.json";
+import initReducer from "./Store/initReducer";
+import initSagas from "./Store/initSagas";
 
 export default {
-  name: 'Cart',
-  dir: 'Cart',
-  pathRoot: 'cart',
+  name: "Cart",
+  dir: "Cart",
+  pathRoot: "cart",
   routes: [
     {
-      url: '',
-      component: 'Page/Carriage',
+      url: "",
+      component: "Page/Carriage",
       meta: {
         authRoute: true,
       },
       props: {
-        title: 'Trang chủ | Hita Camp',
-        titleI18n: 'Cart:title',
-        headerStyle: 'fill',
+        title: "Trang chủ | Hita Camp",
+        titleI18n: "Cart information",
+        headerStyle: "fill",
         exact: true,
       },
     },
   ],
   lang: { vi: intlMessagesVi, en: intlMessagesEN },
-  isAuthenticate: 'Any',
+  isAuthenticate: "Any",
   redux: initReducer,
   sagas: initSagas,
 };
--- src/Modules/Checkout/bootstrap.js
+++ src/Modules/Checkout/bootstrap.js
@@ -1,29 +1,29 @@
-import intlMessagesEN from './i18n/localization/en.json';
-import intlMessagesVi from './i18n/localization/vi.json';
-import initReducer from './Store/initReducer';
-import initSagas from './Store/initSagas';
+import intlMessagesEN from "./i18n/localization/en.json";
+import intlMessagesVi from "./i18n/localization/vi.json";
+import initReducer from "./Store/initReducer";
+import initSagas from "./Store/initSagas";
 
 export default {
-  name: 'Checkout',
-  dir: 'Checkout',
-  pathRoot: 'checkout',
+  name: "Checkout",
+  dir: "Checkout",
+  pathRoot: "checkout",
   routes: [
     {
-      url: '',
-      component: 'Page/CheckOut',
+      url: "",
+      component: "Page/CheckOut",
       meta: {
         authRoute: true,
       },
       props: {
-        title: 'Trang chủ | Hita Camp',
-        titleI18n: 'Home:title',
-        headerStyle: 'fill',
+        title: "Trang chủ | Hita Camp",
+        titleI18n: "Checkout",
+        headerStyle: "fill",
         exact: true,
       },
     },
   ],
   lang: { vi: intlMessagesVi, en: intlMessagesEN },
-  isAuthenticate: 'Any',
+  isAuthenticate: "Any",
   redux: initReducer,
   sagas: initSagas,
 };
